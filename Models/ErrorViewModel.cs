f51ca02258727136c06d5c8aafa12764181f9be1
MrDark-GG <Mr.Dark_GG@protonmail.ch>
2021-05-25 17:14:36 -0500

    Creacion MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PC_N3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
